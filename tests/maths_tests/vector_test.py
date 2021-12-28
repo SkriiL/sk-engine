1d80ca09f0ac0ef4253c29487218c1831ec24188
SkriiL <flx.grimm@gmail.com>
2021-12-28 14:23:24 +0100

    Enhancement #8 - Vector Tests

---
+++ tests/maths_tests/vector_test.py
@@ -0,0 +1,56 @@
+import unittest
+
+
+from maths.vector import Vector
+
+
+class VectorTest(unittest.TestCase):
+    def test_unit(self):
+        self.assertEqual(Vector.unit(), Vector(1, 1, 1))
+
+    def test_add(self):
+        v1 = Vector(1, 2, 3)
+        v2 = Vector(3, 2, 1)
+        self.assertEqual(v1 + v2, Vector(4, 4, 4))
+        v1 += v2
+        self.assertEqual(v1, Vector(4, 4, 4))
+
+    def test_sub(self):
+        v1 = Vector(3, 2, 1)
+        v2 = Vector(5, 4, 3)
+        self.assertEqual(v1 - v2, Vector(-2, -2, -2))
+        v1 -= v2
+        self.assertEqual(v1, Vector(-2, -2, -2))
+
+    def test_scalar_multiplication(self):
+        v1 = Vector.unit()
+        self.assertEqual(v1 * 3, Vector(3, 3, 3))
+        v1 *= 3
+        self.assertEqual(v1, Vector(3, 3, 3))
+
+    def test_scalar_division(self):
+        v1 = Vector(3, 3, 3)
+        self.assertEqual(v1 / 3, Vector.unit())
+        v1 /= 3
+        self.assertEqual(v1, Vector.unit())
+
+    def test_dot_product(self):
+        v1 = Vector(1, 2, 3)
+        v2 = Vector(3, 2, 1)
+        self.assertEqual(v1.dot(v2), 10)
+
+    def test_cross_product(self):
+        v1 = Vector(1, 2, 3)
+        v2 = Vector(3, 2, 1)
+        self.assertEqual(v1 * v2, Vector(-4, 8, -4))
+        v1 *= v2
+        print(v1)
+        self.assertEqual(v1, Vector(-4, 8, -4))
+
+    def test_magnitude(self):
+        v1 = Vector(4, 3, 0)
+        self.assertEqual(abs(v1), 5)
+
+
+if __name__ == '__main__':
+    unittest.main()

6bb3b4432d646627b9d06a9796975d4009dbfe25
SkriiL <flx.grimm@gmail.com>
2021-12-28 11:13:56 +0100

    Enhancement #2 - Addition / Subtraction

--- maths/vector.py
+++ maths/vector.py
@@ -11,3 +11,31 @@ class Vector:
         if isinstance(other, Vector):
             return self.x == other.x and self.y == other.y and self.z == other.z
         return False
+
+    def __add__(self, other) -> "Vector":
+        if isinstance(other, Vector):
+            return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
+        raise TypeError(
+            f"unsupported operand type(s) for +: '{self.__class__.__name__}' and '{other.__class__.__name__}'")
+
+    def __iadd__(self, other):
+        if isinstance(other, Vector):
+            self.x += other.x
+            self.y += other.y
+            self.z += other.z
+        raise TypeError(
+            f"unsupported operand type(s) for +=: '{self.__class__.__name__}' and '{other.__class__.__name__}'")
+
+    def __sub__(self, other) -> "Vector":
+        if isinstance(other, Vector):
+            return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
+        raise TypeError(
+            f"unsupported operand type(s) for -: '{self.__class__.__name__}' and '{other.__class__.__name__}'")
+
+    def __isub__(self, other):
+        if isinstance(other, Vector):
+            self.x += other.x
+            self.y += other.y
+            self.z += other.z
+        raise TypeError(
+            f"unsupported operand type(s) for -=: '{self.__class__.__name__}' and '{other.__class__.__name__}'")

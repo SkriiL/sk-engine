62844783456aa4d146fcb2a62fc7410c48d66328
SkriiL <flx.grimm@gmail.com>
2021-12-29 09:51:10 +0100

    Enhancement #21 - Quaternion Addition / Subtraction

--- maths/quaternion.py
+++ maths/quaternion.py
@@ -1,4 +1,5 @@
 import maths
+from errors import SkTypeError
 
 
 class Quaternion:
@@ -13,6 +14,30 @@ class Quaternion:
 
     def __str__(self) -> str:
         return f"Quaternion({self.scalar}, {self.vector})"
+
+    def __add__(self, other) -> "Quaternion":
+        if isinstance(other, Quaternion):
+            return Quaternion(self.scalar + other.scalar, self.vector + other.vector)
+        raise SkTypeError(self, other, "+")
+
+    def __iadd__(self, other) -> "Quaternion":
+        if isinstance(other, Quaternion):
+            self.scalar += other.scalar
+            self.vector += other.vector
+            return self
+        raise SkTypeError(self, other, "+=")
+
+    def __sub__(self, other) -> "Quaternion":
+        if isinstance(other, Quaternion):
+            return Quaternion(self.scalar - other.scalar, self.vector - other.vector)
+        raise SkTypeError(self, other, "-")
+
+    def __isub__(self, other) -> "Quaternion":
+        if isinstance(other, Quaternion):
+            self.scalar -= other.scalar
+            self.vector -= other.vector
+            return self
+        raise SkTypeError(self, other, "-=")
 
 
 if __name__ == "__main__":
